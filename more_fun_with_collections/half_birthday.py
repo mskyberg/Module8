3be4735d40e18bf3df1fa18971ab84d93adc47b3
mskyberg <mskyberg@gmail.com>
2020-06-18 20:44:22 -0500

    Python Datetime assignment initial commit

---
+++ more_fun_with_collections/half_birthday.py
@@ -0,0 +1,23 @@
+"""
+Program: half_birthday.py
+Author: Michael Skyberg, mskyberg@dmacc.edu
+Last date modified: June 2020
+
+Purpose: demonstrate the use of DateTime
+"""
+import datetime
+
+
+def half_birthday(date):
+    """
+    Description: calculate the next half birthday from provided date
+
+    :param date: this will be the birthday datetime object
+    :returns: return half-birthday datetime object
+    :raises
+    """
+    pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ test/test_half_birthday.py
@@ -0,0 +1,22 @@
+"""
+Program: test_half_birthday.py
+Author: Michael Skyberg, mskyberg@dmacc.edu
+Last date modified: June 2020
+
+Purpose: test the datetime functionality of half birthday calculation
+"""
+
+import datetime
+import unittest
+from more_fun_with_collections import half_birthday as hbd
+
+
+class MyTestCase(unittest.TestCase):
+    def test_average(self):
+        birthday = datetime.datetime(2020, 6, 22)
+        expected_half = datetime.datetime(2020, 12, 22)
+        self.assertEqual(expected_half, hbd.half_birthday(birthday))
+
+
+if __name__ == '__main__':
+    unittest.TestCase()
